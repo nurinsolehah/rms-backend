c95a58c13906a9ccc891721e9fd8ddb370b9e5d4
nurinsolehah <nurinsolehah28@gmail.com>
2025-04-25 11:27:55 +0800

    test

---
+++ src/test/java/com/cmg/rms/rms_backend/RmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmg.rms.rms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
